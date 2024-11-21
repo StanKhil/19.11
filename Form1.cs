4a9e07e239edd5fc5de0ae5848030bd5afaba4b6
Stanislav <stankhil@gmail.com>
2024-11-21 11:49:08 +0200

    исправил ошибки

--- Form1.cs
+++ Form1.cs
@@ -5,17 +5,26 @@ namespace _19._11
         public Form1()
         {
             InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint); // Привязка события Paint
         }
-
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.FillRectangle(Brushes.Red, 50, 50, 300, 200);
-            g.FillRectangle(Brushes.White, 110, 50, 30, 200);
-            g.FillRectangle(Brushes.White, 50, 135, 300, 30);
-            g.FillRectangle(Brushes.Blue, 115, 50, 20, 200);
-            g.FillRectangle(Brushes.Blue, 50, 140, 300, 20);
+            Brush redBrush = new SolidBrush(Color.Red);
+            Brush whiteBrush = new SolidBrush(Color.White);
+            Brush blueBrush = new SolidBrush(Color.Blue);
+            Pen blackPen = new Pen(Color.Black, 2);
+            g.FillRectangle(redBrush, 50, 50, 300, 200);
+            g.FillRectangle(whiteBrush, 110, 50, 30, 200);
+            g.FillRectangle(whiteBrush, 50, 135, 300, 30);
+            g.FillRectangle(blueBrush, 115, 50, 20, 200);
+            g.FillRectangle(blueBrush, 50, 140, 300, 20);
+            g.DrawRectangle(blackPen, 50, 50, 300, 200);
+            redBrush.Dispose();
+            whiteBrush.Dispose();
+            blueBrush.Dispose();
+            blackPen.Dispose();
         }
     }
 }
